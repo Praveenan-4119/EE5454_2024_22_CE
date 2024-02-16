08917983d64e2ce808bfd6f8b28ff519f5ec834a
praveenan <praveenanjvp@gmail.com>
2024-02-16 15:00:28 +0530

    Can function

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def cat():
+    print('Mew')
+
 def default():
     print('Hello')
 
 def main():
-    default()
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
